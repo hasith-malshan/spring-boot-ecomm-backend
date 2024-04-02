0cdabe87f928a92b28f650ff3b09485a152609dd
hmr53 <h.malshan.53@gmail.com>
2024-04-02 15:53:32 +0530

    Limitations added to product category.

--- spring-boot-ecommerce/src/main/java/com/hasith/springbootecommerce/config/MyDataRestConfig.java
+++ spring-boot-ecommerce/src/main/java/com/hasith/springbootecommerce/config/MyDataRestConfig.java
@@ -1,6 +1,9 @@
 package com.hasith.springbootecommerce.config;
 
 import com.hasith.springbootecommerce.entity.Product;
+import com.hasith.springbootecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -9,12 +12,24 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 //        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
         HttpMethod[] theUnsupportedAction = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction));
     }
