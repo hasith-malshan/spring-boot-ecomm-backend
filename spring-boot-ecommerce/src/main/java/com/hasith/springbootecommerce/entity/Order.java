69152caa484343886eea44cac6fa4a2a3192c49a
hmr53 <h.malshan.53@gmail.com>
2024-12-02 07:32:53 +0530

    Created New Entities for Order process.

---
+++ spring-boot-ecommerce/src/main/java/com/hasith/springbootecommerce/entity/Order.java
@@ -0,0 +1,44 @@
+
+package com.hasith.springbootecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+
+    @Column(name = "order_tracking_number")
+    private String orderTrackingNumber;
+
+    @Column(name = "total_quantity")
+    private Integer totalQuantity;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+
+}
---
+++ spring-boot-ecommerce/src/main/java/com/hasith/springbootecommerce/entity/OrderItem.java
@@ -0,0 +1,32 @@
+package com.hasith.springbootecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+public class OrderItem {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+}
